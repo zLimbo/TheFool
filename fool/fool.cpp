8fccf5ede2b695972c531c3d048bc97c580d3b32
zLimbo <67814424+zLimbo@users.noreply.github.com>
2023-06-26 07:25:33 +0000

    add submodule fmt

--- fool/fool.cpp
+++ fool/fool.cpp
@@ -1,12 +1,12 @@
+#include "fmt/format.h"
 
-
-auto f(auto x) {
-    return static_cast<double>(x);
-}
+auto f(auto x) { return static_cast<double>(x); }
 
 int main() {
 
-    auto x = f(3);
+  auto x = f(3);
 
-    return 0;
+  fmt::println("x:{}", x);
+
+  return 0;
 }
